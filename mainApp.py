e4379039d83aa74875e05c98db2ef258c5632b56
Elwin Hammer <elwinhammer@gmail.com>
2021-07-26 13:54:02 +0200

    First set-up

---
+++ mainApp.py
@@ -0,0 +1,19 @@
+#import serial
+#ser = serial.Serial('/dev/ttyAMA0', 4800, timeout=1)
+#print(ser)
+
+#ser.write(str.encode(':0603000C000145BE\r\n'))
+#print(repr(ser.read(1000)))  # Read 1000 bytes, or wait for timeout
+
+
+
+
+
+import serial
+
+ser = serial.Serial('/dev/ttyAMA0', 4800, timeout=4)
+cmd = [0x03, 0x03, 0x00, 0x00, 0x00, 0x01, 0x84, 0x0A]
+ser.write(serial.to_bytes(cmd))
+response = ser.read(128)
+
+print(response.hex())
