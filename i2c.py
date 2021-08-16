1cb71f944e4e4fcdd116d32e644941b02154b5fe
Elwin Hammer <elwinhammer@gmail.com>
2021-08-16 15:24:26 +0200

    sps30 driver call change

--- i2c.py
+++ i2c.py
@@ -2,6 +2,6 @@ import sys
 # insert at 1, 0 is the script path (or '' in REPL)
 sys.path.append('/meetfietsApp/Raspi-Driver-SPS30')
 import sps30Service
-os.system("/meetfietsApp/Raspi-Driver-SPS30/sps30-service.py 1")
+#os.system("/meetfietsApp/Raspi-Driver-SPS30/sps30Service.py 1")
 if rdy == 1:
     print('OKE')
