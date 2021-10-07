6a25003317d7faa112af7ce8e4e14cdae1a2567c
Elwin Hammer <elwinhammer@gmail.com>
2021-10-07 15:32:28 +0200

    Other small changes to the SPS30 driver

--- sps30-service.py
+++ sps30-service.py
@@ -270,16 +270,16 @@ def printHuman(data):
   print("pm_typ: %f" % calcFloat(data[54:60]))
 
 def putInConfigFile(data):
-    config['pm05']['count'] = calcFloat(data[24:30])
-    config['pm1']['count'] = calcFloat(data[30:36])  
-    config['pm1']['ug'] = calcFloat(data[6:12])
-    config['pm25']['count'] = calcFloat(data[42:48])
-    config['pm25']['ug'] = calcFloat(data[6:12])
-    config['pm4']['count'] = calcFloat(data[42:48])
-    config['pm4']['ug'] = calcFloat(data[12:18])
-    config['pm10']['count'] = calcFloat(data[48:54])
-    config['pm10']['ug'] = calcFloat(data[18:24])
-    config['pm']['type'] = calcFloat(data[54:60])
+    config['pm05']['count'] = str(int(calcFloat(data[24:30])))
+    config['pm1']['count'] = str(int(calcFloat(data[30:36])))  
+    config['pm1']['ug'] = str(int(calcFloat(data[6:12])))
+    config['pm25']['count'] = str(int(calcFloat(data[42:48])))
+    config['pm25']['ug'] = str(int(calcFloat(data[6:12])))
+    config['pm4']['count'] = str(int(calcFloat(data[42:48])))
+    config['pm4']['ug'] = str(int(calcFloat(data[12:18])))
+    config['pm10']['count'] = str(int(calcFloat(data[48:54])))
+    config['pm10']['ug'] = str(int(calcFloat(data[18:24])))
+    config['pm']['type'] = str(int(calcFloat(data[54:60])))
     with open(configDataPath, 'w') as configfile:    # save
         config.write(configfile)
 
