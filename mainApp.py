d1c7397bd83f17ff84951df6a202e310d2f24490
Elwin Hammer <elwinhammer@gmail.com>
2021-11-04 00:29:43 +0100

    Some small bug fixes

--- mainApp.py
+++ mainApp.py
@@ -15,11 +15,11 @@ import socket
 
 #SETUP  
 config = configparser.ConfigParser()
-version = '0.1.7'
+version = '0.1.8'
 
 #Settings
 try:
-    config.read('settings.ini') #CHANGE THIS IF YOU HAVE MOVED THE SETTINGS.INI FILE!!! DEFAULT: /meetfietsApp/Meetfiets/settings.ini
+    config.read('/meetfietsApp/Meetfiets/settings.ini') #CHANGE THIS IF YOU HAVE MOVED THE SETTINGS.INI FILE!!! DEFAULT: /meetfietsApp/Meetfiets/settings.ini
     print(config.sections())
 except Exception as e: print("ERROR: ", e) 
 
@@ -65,7 +65,7 @@ def main():
         sensorID = ["02","03","04","05","06","07","69","70","71","72","73"]
         sensorType = ["ND","BC","OZ","SO","DB","NO","PM","PM","PM","PM","PM"]
         sensorFunc = [rs485_crc16.getNO2(debug),rs485_crc16.getBC(debug),rs485_crc16.getO3(debug),rs485.getSO2(debug),rs485.getdB(debug),rs485.getNO(debug),i2c.getPM05_count(sps30dataPath, debug),i2c.getPM1_count(sps30dataPath, debug),i2c.getPM25_count(sps30dataPath, debug),i2c.getPM4_count(sps30dataPath, debug),i2c.getPM10_count(sps30dataPath, debug)]
-        sensorSpecialsA = [sen_NO2,sen_BC,sen_O3,]
+        sensorSpecialsA = [sen_NO2,sen_BC,sen_O3]
         sensorNorm = [sen_SO2,sen_dB,sen_NO]
         sensorSpecialsB = [sen_PM05,sen_PM1,sen_PM25,sen_PM4,sen_PM10]
 
@@ -86,52 +86,87 @@ def main():
 
 
         print("list: " + str(sensorSpecialsB) + "\n")
-        for s in sensorSpecialsB:
-                if s != "":
-                    val = sensorSpecialsB[sensorSpecialsB.index(s)] * 100
-                    if debug == '2':
-                        print("hex: " + hex(int(s)) + "\n")                 
-                    val = hex(int(s))     
-                    newVal =  val[2:]
-                    if debug == '2':
-                        print("new val: ", newVal, "\n")
-                    if len(newVal) == 1:
-                        newVal = "0" + newVal
-                    newData = '00 00 00 '+newVal+' 00 00'
-                    if debug == '2':
-                        print("New data: " + str(newData) + "\n")
-                    sensorSpecialsB[sensorSpecialsB.index(s)] = newData
+        try:
+            for s in sensorSpecialsB:
+                    if s != "":
+                        val = sensorSpecialsB[sensorSpecialsB.index(s)] * 100
+                        if debug == '2':
+                            print("hex: " + hex(int(s)) + "\n")                 
+                        val = hex(int(s))     
+                        newVal =  val[2:]
+                        if debug == '2':
+                            print("new val: ", newVal, "\n")
+                        if len(newVal) == 1:
+                            newVal = "0" + newVal
+                        newData = '00 00 00 '+newVal+' 00 00'
+                        if debug == '2':
+                            print("New data: " + str(newData) + "\n")
+                        sensorSpecialsB[sensorSpecialsB.index(s)] = newData
+        except Exception as u:
+            now = datetime.now()
+            # dd/mm/YY H:M:S
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print("ERROR: ", u, exc_type, fname, exc_tb.tb_lineno)
+            msg = dt_string + "|ERROR: " + str(u) + " " +  str(exc_type) + " " + str(fname) + "Line: " + str(exc_tb.tb_lineno) +  "\n" 
+            logFile = open(logFilePath, 'a')
+            logFile.write(msg)
+            logFile.close()
 
-        for m in sensorSpecialsA:
-                if m != "":
-                    val = sensorSpecialsA[sensorSpecialsA.index(m)] 
-                    if len(val) == 1:
-                        val = "0" + val
-                    newData = '00 00 00 '+val+' 00 00'
-                    if debug == '2':
-                        print("New data: " + str(newData) + "\n")
-                    sensorSpecialsA[sensorSpecialsA.index(m)] = newData
-        while('' in sensorSpecialsA) :
-            sensorSpecialsA.remove('')
-        while('' in sensorSpecialsB) :
-            sensorSpecialsB.remove('')
-
-        while('' in sensorNorm) :
-            sensorNorm.remove('')
-        del sensorNorm[:3]
-        del sensorNorm[3:]
-        newSenVal = sensorSpecialsA + sensorNorm + sensorSpecialsB
-        if debug == '2':
-            print("sensorSpecialsA len: ", len(sensorSpecialsA), "\n")
-            print("sensorNorm len: ", len(sensorNorm), "\n")
-            print("sensorSpecialsB len: ", len(sensorSpecialsB), "\n")
-            print("sensorSpecialsA: ", sensorSpecialsA, "\n")
-            print("sensorNorm: ", sensorNorm, "\n")
-            print("sensorSpecialsB: ", sensorSpecialsB, "\n")
-            print("NEW DATA LIST: ", newSenVal, "\n")
-        now = datetime.utcnow()
-        dateTime = now.strftime("%Y-%m-%dT%H:%M:%SZ")
-        api.convertData(logFilePath, newSenVal, fiets, sensorID, sensorType, dateTime, urlAPI, debug)
+        try:
+            for m in sensorSpecialsA:
+                    if m != "":
+                        val = sensorSpecialsA[sensorSpecialsA.index(m)] 
+                        if len(val) == 1:
+                            val = "0" + val
+                        newData = '00 00 00 '+val+' 00 00'
+                        if debug == '2':
+                            print("New data: " + str(newData) + "\n")
+                        sensorSpecialsA[sensorSpecialsA.index(m)] = newData
+            while('' in sensorSpecialsA) :
+                sensorSpecialsA.remove('')
+            while('' in sensorSpecialsB) :
+                sensorSpecialsB.remove('')
+        except Exception as u:
+            now = datetime.now()
+            # dd/mm/YY H:M:S
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print("ERROR: ", u, exc_type, fname, exc_tb.tb_lineno)
+            msg = dt_string + "|ERROR: " + str(u) + " " +  str(exc_type) + " " + str(fname) + "Line: " + str(exc_tb.tb_lineno) +  "\n" 
+            logFile = open(logFilePath, 'a')
+            logFile.write(msg)
+            logFile.close()
+        try:
+            while('' in sensorNorm) :
+                sensorNorm.remove('')
+            del sensorNorm[:3]
+            del sensorNorm[3:]
+            newSenVal = sensorSpecialsA + sensorNorm + sensorSpecialsB
+            if debug == '2':
+                print("sensorSpecialsA len: ", len(sensorSpecialsA), "\n")
+                print("sensorNorm len: ", len(sensorNorm), "\n")
+                print("sensorSpecialsB len: ", len(sensorSpecialsB), "\n")
+                print("sensorSpecialsA: ", sensorSpecialsA, "\n")
+                print("sensorNorm: ", sensorNorm, "\n")
+                print("sensorSpecialsB: ", sensorSpecialsB, "\n")
+                print("NEW DATA LIST: ", newSenVal, "\n")
+            now = datetime.utcnow()
+            dateTime = now.strftime("%Y-%m-%dT%H:%M:%SZ")
+            api.convertData(logFilePath, newSenVal, fiets, sensorID, sensorType, dateTime, urlAPI, debug)
+        except Exception as u:
+            now = datetime.now()
+            # dd/mm/YY H:M:S
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print("ERROR: ", u, exc_type, fname, exc_tb.tb_lineno)
+            msg = dt_string + "|ERROR: " + str(u) + " " +  str(exc_type) + " " + str(fname) + "Line: " + str(exc_tb.tb_lineno) +  "\n" 
+            logFile = open(logFilePath, 'a')
+            logFile.write(msg)
+            logFile.close()
     except Exception as u:
         now = datetime.now()
         # dd/mm/YY H:M:S
