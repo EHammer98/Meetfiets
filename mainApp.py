f3a64d429fd72e008025180c01c43c02211628ab
Elwin Hammer <elwinhammer@gmail.com>
2021-12-16 23:50:09 +0100

    Minor bug fixes
    No correct data conversion for sensorA group.
    
    PM sensors(B) are now converted to HEX

--- mainApp.py
+++ mainApp.py
@@ -19,7 +19,7 @@ me = singleton.SingleInstance() # will sys.exit(-1) if other instance is running
 
 #SETUP  
 config = configparser.ConfigParser()
-version = '0.1.9.7'
+version = '0.1.9.8'
 
 #Settings
 try:
@@ -95,7 +95,7 @@ def main():
                         val = sensorSpecialsA[sensorSpecialsA.index(s)] * 100
                         if debug == '2':
                             print("hex: " + hex(int(s)) + "\n")                 
-                        val = hex(int(s))     
+                        val = hex(int(val))     
                         newVal =  val[2:]
                         if debug == '2':
                             print("new val: ", newVal, "\n")
@@ -125,7 +125,7 @@ def main():
                     if m != "":
                         val = sensorSpecialsB[sensorSpecialsB.index(m)] 
                         if len(str(val)) == 1:
-                            val = "0" + val
+                            val = hex(int("0" + val))                         
                         newData = '00 00 00 '+val+' 00 00'
                         if debug == '2':
                             print("New data: " + str(newData) + "\n")
