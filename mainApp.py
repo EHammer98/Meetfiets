6079a2024de2061dcf728c8c87032b3c1770ccba
Elwin Hammer <elwinhammer@gmail.com>
2021-12-22 23:50:28 +0100

    Change in hex conversion

--- mainApp.py
+++ mainApp.py
@@ -19,7 +19,7 @@ me = singleton.SingleInstance() # will sys.exit(-1) if other instance is running
 
 #SETUP  
 config = configparser.ConfigParser()
-version = '0.1.9.9'
+version = '0.2.0.0'
 
 #Settings
 try:
@@ -125,7 +125,9 @@ def main():
                     if m != "":
                         val = sensorSpecialsB[sensorSpecialsB.index(m)] 
                         if len(str(val)) == 1:
-                            val = format(int(val),'X')                        
+                            val = hex(int(val))      
+                            if (val.find('x') != -1):
+                                val = val.replace("x", "")
                         newData = '00 00 00 '+val+' 00 00'
                         if debug == '2':
                             print("New data: " + str(newData) + "\n")
